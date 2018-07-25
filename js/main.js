98bac7d1f77ba9512eb82c1899cc7814a774eb9c
ricky-bruner <alteredecho07@yahoo.com>
2018-07-25 16:34:23 -0500

    MEG

--- js/main.js
+++ js/main.js
@@ -35,49 +35,43 @@ mainContent.innerHTML +=
             <div>
                 <h3>${data.general[1].name}</h3>
                 <ul>
-                    <li>${data.general[1].statements[0]}<br><small>${data.general[2].name}${data.general[2].details[0]}</small></li>
-                    <li>${data.general[1].statements[1]}<br><small>${data.general[2].name}${data.general[2].details[1]}</small></li>
-                    <li>${data.general[1].statements[2]}<br><small>${data.general[2].name}${data.general[2].details[2]}</small></li>
+                    <li>${data.general[1].statements[0]}<br><small>${data.general[2].name} ${data.general[2].details[0]}</small></li>
+                    <li>${data.general[1].statements[1]}<br><small>${data.general[2].name} ${data.general[2].details[1]}</small></li>
+                    <li>${data.general[1].statements[2]}<br><small>${data.general[2].name} ${data.general[2].details[2]}</small></li>
                 </ul>
             </div>
             
         </div>
     </div>`
 
-mainContent.innerHTML +=
-    `<h2>People Like Me!</h2>
-    <div class="people-container">
-        <div>
-            <img src="${data.peopleLikeMe[0].image}" alt="${data.peopleLikeMe[0].name}">
-            <h4>${data.peopleLikeMe[0].name}</h4>
-        </div>
-        <div>
-            <img src="${data.peopleLikeMe[1].image}" alt="${data.peopleLikeMe[1].name}">
-            <h4>${data.peopleLikeMe[1].name}</h4>
-        </div>
-        <div>
-            <img src="${data.peopleLikeMe[2].image}" alt="${data.peopleLikeMe[2].name}">
-            <h4>${data.peopleLikeMe[2].name}</h4>
-        </div>
-        <div>
-            <img src="${data.peopleLikeMe[3].image}" alt="${data.peopleLikeMe[3].name}">
-            <h4>${data.peopleLikeMe[3].name}</h4>
-        </div>
-    </div>`   
-    
-mainContent.innerHTML += 
-    `<h2>Communication: Me, You, and Us!</h2>
-     <div class="communication-container">
-        <div>
-            <h4>${data.communication[0].name}</h4>
-            <p>${data.communication[0].details}</p>
-        </div>
-        <div>
-            <h4>${data.communication[1].name}</h4>
-            <p>${data.communication[1].details}</p>
-        </div>
-        <div>
-            <h4>${data.communication[2].name}</h4>
-            <p>${data.communication[2].details}</p>
-        </div>
-     </div>`
+function addPeopleLikeMe(){
+    mainContent.innerHTML +=
+        `<h2>People Like Me!</h2>
+        <div class="people-container">
+        </div>`
+    let peopleDiv = document.querySelector(".people-container");
+    for (let i = 0; i < data.peopleLikeMe.length; i++) {
+        peopleDiv.innerHTML += 
+            `<div>
+                <img src="${data.peopleLikeMe[i].image}" alt="${data.peopleLikeMe[i].name}">
+                <h4>${data.peopleLikeMe[i].name}</h4>
+            </div>`
+    } 
+}
+
+addPeopleLikeMe();
+
+function addCommunication(){
+    mainContent.innerHTML += 
+        `<h2>Communication: Me, You, and Us!</h2>
+        <div class="communication-container"></div>`
+    let commDiv = document.querySelector(".communication-container");
+    for (let i = 0; i < data.communication.length; i++) {
+        commDiv.innerHTML +=
+            `<div>
+                <h4>${data.communication[i].name}</h4>
+                <p>${data.communication[i].details}</p>
+            </div>`
+    }
+}
+addCommunication();
